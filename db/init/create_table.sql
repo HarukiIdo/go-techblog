0bbae6005d27161eb62625d8a9db985d03d78ff9
Haruki Ido <rikuharu3705@outlook.jp>
2022-06-12 15:03:47 +0900

    fix bug of internal server error

--- db/init/create_table.sql
+++ db/init/create_table.sql
@@ -1,9 +1,14 @@
-CREATE DATABASE IF NOT EXISTS go_db;
+DROP DATABASE IF EXISTS go_db
+
+CREATE DATABASE go_db;
 
 use go_db;
 
 CREATE TABLE IF NOT EXISTS articles(
   id int AUTO_INCREMENT,
   title varchar(100),
+  body mediumtext NOT NULL,
+  created datetime,
+  updated datetime,
   PRIMARY KEY(id)
 ); 
