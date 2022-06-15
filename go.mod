f56a0da736e9214892691217b0e37c6d46dc0439
Haruki Ido <rikuharu3705@outlook.jp>
2022-06-16 01:45:31 +0900

    add repository test code with failure test

--- go.mod
+++ go.mod
@@ -3,16 +3,24 @@ module github.com/HarukiIdo/go-techblog
 go 1.17
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0
+	github.com/stretchr/testify v1.7.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/lib/pq v1.10.6 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-sqlite3 v1.14.13 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
 
 require (
--- repository/article_repository_test.go
+++ repository/article_repository_test.go
@@ -1,20 +1,46 @@
 package repository
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/HarukiIdo/go-techblog/model"
+	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestArticle_Create(t *testing.T) {
 
-	// t.Run(
-	// 	"清浄系：エラーなし",
-	// 	func (t *testing.T)  {
-	// 		article := &model.Article{
-	// 			Title: "article title",
-	// 			Body: "article body",
-	// 		}
+	t.Run(
+		"正常系：エラーなし",
+		func(t *testing.T) {
+			article := &model.Article{
+				Title: "article title",
+				Body:  "article body",
+			}
 
-	// 		//db,modk,err:=
-	// 	}
-	// )
+			// モック用のコネクションを作成
+			db, mock, err := sqlmock.New()
+			assert.NoError(t, err)
+			defer db.Close()
+			dbx := sqlx.NewDb(db, "mysql")
+
+			// SQLのクエリの引数と戻り値が期待値と一致するか
+			mock.ExpectPrepare(`INSERT INTO articles`).ExpectExec().WithArgs(article.Title, article.Body, article.CreatedAt, article.UpdatedAt).WillReturnResult(sqlmock.NewResult(1, 1))
+
+			// テスト対象のリポジトリを作成
+			r := NewArticleRepository(dbx)
+
+			res, err := r.ArticleCreate(article)
+			fmt.Println(res)
+
+			// エラーが発生しないことを期待
+			assert.NoError(t, err)
+
+			// 上記で指定した通りにモックが呼ばれることを期待
+			assert.NoError(t, mock.ExpectationsWereMet())
+
+		},
+	)
 }
